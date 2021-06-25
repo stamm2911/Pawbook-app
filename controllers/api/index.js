3594f2f97e1ff6d27d803df3263155f83a6b1188
Eduardo Stamm <edstamm29@gmail.com>
2021-06-25 17:43:40 -0500

    session updates

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const adoptionRoutes = require('./adoptionRoutes');
 const lost_animalRoutes = require('./lost_animalRoutes');
 const foodRoutes = require('./foodRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/adoption',adoptionRoutes);
 router.use('/lost_animal',lost_animalRoutes);
 router.use('/food',foodRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,38 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const uniqid = require('uniqid');
 
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
+
+// Default option fileUpload
+app.use(fileUpload());
+
+// Static files
+app.use(express.static("public"));
+app.use(express.static("upload"));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
